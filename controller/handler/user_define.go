542c3f0a94bf46d7d26ddd50607f37e9097d8916
yuha-yuha <ishizushimadev@gmail.com>
2024-07-01 21:58:18 +0900

    update:
    user定義のハンドラー

--- controller/handler/user_define.go
+++ controller/handler/user_define.go
@@ -1,8 +1,33 @@
 package handler
 
-import "net/http"
+import (
+	"encoding/json"
+	"net/http"
 
-type UserDefine struct {
-	Path    string
-	Handler http.Handler
+	"github.com/yuha-yuha/DevMomentAPI/helper"
+)
+
+type UserDefineHandler struct {
+	Path        string `json:"path"`
+	HandlerFunc http.HandlerFunc
+}
+
+func GetUserDefineHandlers(jaf helper.JsonAPIFormat) []UserDefineHandler {
+	userDefHandlers := []UserDefineHandler{}
+	for _, UserDefineAPI := range jaf.UserDefineAPIs {
+		handlerFunc := func(w http.ResponseWriter, r *http.Request) {
+			w.Header().Set("Content-Type", "application/json; charset=utf-8")
+			jsonenc := json.NewEncoder(w)
+			jsonenc.Encode(UserDefineAPI.Response)
+		}
+
+		userDefHandler := UserDefineHandler{
+			Path:        UserDefineAPI.Path,
+			HandlerFunc: handlerFunc,
+		}
+
+		userDefHandlers = append(userDefHandlers, userDefHandler)
+	}
+
+	return userDefHandlers
 }
