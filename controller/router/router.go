68c6d997ea431c6ab0a502ebf831885c110442d6
yuha-yuha <ishizushimadev@gmail.com>
2024-07-01 21:58:48 +0900

    handlerをmuxに入れる処理

--- controller/router/router.go
+++ controller/router/router.go
@@ -1,9 +1,19 @@
 package router
 
-import "net/http"
+import (
+	"net/http"
+
+	"github.com/yuha-yuha/DevMomentAPI/controller/handler"
+	"github.com/yuha-yuha/DevMomentAPI/helper"
+)
 
 func Get() *http.ServeMux {
 	mux := http.NewServeMux()
+
+	uds := handler.GetUserDefineHandlers(helper.JsonParse())
+	for _, ud := range uds {
+		mux.Handle(ud.Path, ud.HandlerFunc)
+	}
 
 	mux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
 		s := "hello"
