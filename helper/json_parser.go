be13f2c7d82fe9ee67a65831ac146b434ff94c1e
yuha-yuha <ishizushimadev@gmail.com>
2024-07-01 21:57:51 +0900

    update:
    json文字列のparse

--- helper/json_parser.go
+++ helper/json_parser.go
@@ -1,8 +1,38 @@
 package helper
 
-type JsonFormat struct {
-	APIs []struct {
-		Path     string                 `json:"path"`
-		Response map[string]interface{} `json:"response"`
-	} `json:"apis"`
+import "encoding/json"
+
+type JsonAPIFormat struct {
+	UserDefineAPIs []UserDefineAPI `json:"apis"`
+}
+
+type UserDefineAPI struct {
+	Path     string                 `json:"path"`
+	Response map[string]interface{} `json:"response"`
+}
+
+func JsonParse() JsonAPIFormat {
+	s :=
+		`{"apis":[
+		{
+			"path":"/hoge",
+			"response": {
+				"message":"hello"
+			}
+		},
+		{
+			"path":"/def",
+			"response": {
+				"message":"aiueo"
+			}
+		}
+	]}`
+
+	sb := []byte(s)
+
+	jaf := JsonAPIFormat{}
+
+	json.Unmarshal(sb, &jaf)
+
+	return jaf
 }
