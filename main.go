741d8eaef087dfc6c32fa1d5f66537048b229204
yuha-yuha <ishizushimadev@gmail.com>
2024-09-16 21:32:29 +0900

    update:main

--- main.go
+++ main.go
@@ -1,17 +1,26 @@
 package main
 
 import (
+	"log"
 	"net/http"
 
-	"github.com/yuha-yuha/DevMomentAPI/controller/router"
+	"github.com/joho/godotenv"
+	"github.com/yuha-yuha/DevMomentAPI/controllers/router"
 )
+
+func init() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+}
 
 func main() {
 
-	mux := router.Get()
+	mux := router.Get("./sample.json")
 
 	server := http.Server{
-		Addr:    ":8081",
+		Addr:    ":8080",
 		Handler: mux,
 	}
 
